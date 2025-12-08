f458ca63a387b83c429c088d06313700df33b4b0
Erica Nyman <ervin.erica29@gmail.com>
2025-12-08 01:06:04 -0500

    feat: updated spacing within the query to avoid errors with seeding data

--- db/queries/guests.js
+++ db/queries/guests.js
@@ -3,7 +3,7 @@ import db from "#db/client";
 export async function createGuest({ user_id, guest_name }) {
   try {
     const sql = `
-        INSERT INTO guests (user_id guest_name)
+        INSERT INTO guests (user_id, guest_name)
         VALUES ($1, $2)
         RETURNING *
         `;
