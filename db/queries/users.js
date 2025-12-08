e411d2868b782a16de9ec3359b052fb7efc0157b
Erica Nyman <ervin.erica29@gmail.com>
2025-12-08 00:17:55 -0500

    feat: updated queries/users to include try/catch blocks and user first_name, last_name, and email

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,46 +1,68 @@
 import db from "#db/client";
 import bcrypt from "bcrypt";
 
-export async function createUser(username, password) {
-  const sql = `
-  INSERT INTO users
-    (username, password)
-  VALUES
-    ($1, $2)
-  RETURNING *
-  `;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const {
-    rows: [user],
-  } = await db.query(sql, [username, hashedPassword]);
-  return user;
+export async function createUser({
+  first_name,
+  last_name,
+  email,
+  username,
+  password,
+}) {
+  try {
+    const sql = `
+    INSERT INTO users
+      (first_name, last_name, email, username, password)
+    VALUES
+      ($1, $2, $3, $4, $5)
+    RETURNING *
+    `;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const values = [first_name, last_name, email, username, hashedPassword];
+    const {
+      rows: [user],
+    } = await db.query(sql, values);
+    return user;
+  } catch (error) {
+    console.error(`Error creating user`, error);
+    throw error;
+  }
 }
 
 export async function getUserByUsernameAndPassword(username, password) {
-  const sql = `
-  SELECT *
-  FROM users
-  WHERE username = $1
-  `;
-  const {
-    rows: [user],
-  } = await db.query(sql, [username]);
-  if (!user) return null;
+  try {
+    const sql = `
+    SELECT *
+    FROM users
+    WHERE username = $1
+    `;
+    const {
+      rows: [user],
+    } = await db.query(sql, [username]);
+    if (!user) return null;
 
-  const isValid = await bcrypt.compare(password, user.password);
-  if (!isValid) return null;
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) return null;
 
-  return user;
+    return user;
+  } catch (error) {
+    console.error(`Error getting user`, error);
+    throw error;
+  }
 }
 
 export async function getUserById(id) {
-  const sql = `
-  SELECT *
-  FROM users
-  WHERE id = $1
-  `;
-  const {
-    rows: [user],
-  } = await db.query(sql, [id]);
-  return user;
+  try {
+    const sql = `
+    SELECT *
+    FROM users
+    WHERE id = $1
+    `;
+    const {
+      rows: [user],
+    } = await db.query(sql, [id]);
+    return user;
+  } catch (error) {
+    console.error(`Error getting user by ID`, error);
+    throw error;
+  }
 }
