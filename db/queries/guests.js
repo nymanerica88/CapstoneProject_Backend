5f9ebdf443307d1250e024d0633053a288eeeafc
Erica Nyman <ervin.erica29@gmail.com>
2025-12-08 00:06:36 -0500

    feat: created query for createGuest

---
+++ db/queries/guests.js
@@ -0,0 +1,20 @@
+import db from "#db/client";
+
+export async function createGuest({ user_id, guest_name }) {
+  try {
+    const sql = `
+        INSERT INTO guests (user_id guest_name)
+        VALUES ($1, $2)
+        RETURNING *
+        `;
+
+    const values = [user_id, guest_name];
+    const {
+      rows: [guest],
+    } = await db.query(sql, values);
+    return guest;
+  } catch (error) {
+    console.error("Error creating new guest", error);
+    throw error;
+  }
+}
