ce291cb1ba93f5f21b4abd38906cdfc5d7e975c6
Erica Nyman <ervin.erica29@gmail.com>
2025-12-11 22:40:27 -0500

    feat: created profileRouter.get

---
+++ api/profile.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  getBillsForGuestProfile,
+  getTotalOwedForGuest,
+} from "#db/queries/z_split_expenses";
+
+const profileRouter = express.Router();
+
+profileRouter.get("/:guest_id", async (req, res, next) => {
+  try {
+    const guest_id = Number(req.params.guest_id);
+    const bills = await getBillsForGuestProfile(guest_id);
+    const totalOwed = await getTotalOwedForGuest(guest_id);
+    if (!bills || bills.length === 0) {
+      return res.status(404).send("Profile not found");
+    }
+    res.json({
+      bills,
+      total_owed: totalOwed,
+    });
+  } catch (error) {
+    console.error(
+      `Error fetching profile for guest ${req.params.guest_id}`,
+      error
+    );
+    next(error);
+  }
+});
