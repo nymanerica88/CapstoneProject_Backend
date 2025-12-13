1c1ab12f290e13415451d71077304fa9f9383ac8
Erica Nyman <ervin.erica29@gmail.com>
2025-12-12 22:34:50 -0500

    feat: added is_user parameter

--- db/queries/guests.js
+++ db/queries/guests.js
@@ -1,14 +1,18 @@
 import db from "#db/client";
 
-export async function createGuest({ user_id, guest_name }) {
+export async function createGuest({
+  user_id = null,
+  guest_name,
+  is_user = false,
+}) {
   try {
     const sql = `
-        INSERT INTO guests (user_id, guest_name)
-        VALUES ($1, $2)
+        INSERT INTO guests (user_id, guest_name, is_user)
+        VALUES ($1, $2, $3)
         RETURNING *
         `;
 
-    const values = [user_id, guest_name];
+    const values = [user_id, guest_name, is_user];
     const {
       rows: [guest],
     } = await db.query(sql, values);
