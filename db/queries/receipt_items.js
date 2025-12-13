7d884add88fe0f5d1c1906cced666385ecedb045
Erica Nyman <ervin.erica29@gmail.com>
2025-12-13 00:41:22 -0500

    feat: updated itemized_receipt to receipt_items (all instances)

--- db/queries/receipt_items.js
+++ db/queries/receipt_items.js
@@ -9,7 +9,7 @@ export async function createReceiptItem({
 }) {
   try {
     const sql = `
-        INSERT INTO itemized_receipt (bill_id, guest_id, item_name, quantity, price)
+        INSERT INTO receipt_items (bill_id, guest_id, item_name, quantity, price)
         VALUES($1, $2, $3, $4, $5)
         RETURNING *   
         `;
@@ -28,11 +28,11 @@ export async function createReceiptItem({
 export async function getItemsForBill(bill_id) {
   try {
     const sql = `
-    SELECT itemized_receipt.item_name, itemized_receipt.quantity, itemized_receipt.price, guests.guest_name
-    FROM itemized_receipt
-    JOIN guests ON guests.id = itemized_receipt.guest_id
-    WHERE itemized_receipt.bill_id = $1
-    ORDER BY itemized_receipt.id    
+    SELECT receipt_items.item_name, receipt_items.quantity, receipt_items.price, guests.guest_name
+    FROM receipt_items
+    JOIN guests ON guests.id = receipt_items.guest_id
+    WHERE receipt_items.bill_id = $1
+    ORDER BY receipt_items.id    
     `;
 
     const values = [bill_id];
