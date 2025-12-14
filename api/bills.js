d178395a426d5d0d0bd485ea061c9758b4c8b5b4
Erica Nyman <ervin.erica29@gmail.com>
2025-12-14 15:25:52 -0500

    feat: initiated db transaction; added createBill logic, added createGuest logic; corrected file path for createReceiptItem import

--- api/bills.js
+++ api/bills.js
@@ -3,5 +3,34 @@ import db from "#db/client";
 import requireUser from "#middleware/requireUser";
 import { createBill } from "#db/queries/bills";
 import { createGuest } from "#db/queries/guests";
-import { createReceiptItem } from "#db/split_expenses";
+import { createReceiptItem } from "#db/receipt_items";
 import { createSplitExpense } from "#db/queries/split_expenses";
+
+const billsRouter = express.Router();
+
+billsRouter.post("/", requireUser, async (req, res, next) => {
+  const { ref_num, total, split_type, guests, items, percentages } = req.body;
+
+  try {
+    await db.query("BEGIN");
+
+    const bill = await createBill({
+      owner_user_id: req.user.id,
+      ref_num,
+      type: split_type,
+      total,
+    });
+
+    const createdGuests = [];
+    for (const guestName of guests) {
+      const guest = await createGuest({
+        user_id: null,
+        guest_name: guestName,
+        is_user: false,
+      });
+      createdGuests.push(guest);
+    }
+  } catch (error) {}
+});
+
+export default billsRouter;
