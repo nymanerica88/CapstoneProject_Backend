ffa155dae2ef58df53f470a638fe72c51882ab3c
Erica Nyman <ervin.erica29@gmail.com>
2025-12-17 05:30:32 -0500

    feat: added updateReceiptItem and deleteReceiptItem

--- db/queries/receipt_items.js
+++ db/queries/receipt_items.js
@@ -43,3 +43,45 @@ export async function getItemsForBill(bill_id) {
     throw error;
   }
 }
+
+export async function updateReceiptItem(
+  item_id,
+  { item_name, quantity, price }
+) {
+  try {
+    const sql = `
+    UPDATE receipt_items
+    SET item_name = $1, quantity=$2, price=$3
+    WHERE id = $4
+    RETURNING *    
+    `;
+
+    const values = [item_name, quantity, price, item_id];
+    const {
+      rows: [item],
+    } = await db.query(sql, values);
+    return item;
+  } catch (error) {
+    console.error(`Error updating receipt item`, error);
+    throw error;
+  }
+}
+
+export async function deleteReceiptItem(item_id) {
+  try {
+    const sql = `
+    DELETE FROM receipt_items
+    WHERE id = $1
+    RETURNING *
+    `;
+
+    const values = [item_id];
+    const {
+      rows: [item],
+    } = await db.query(sql, values);
+    return item;
+  } catch (error) {
+    console.error(`Error deleting receipt item`, error);
+    throw error;
+  }
+}
